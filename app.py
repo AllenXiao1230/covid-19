20de899d0f9e5482e9cf61dbf343b5f193bcec40
Allen is coding <dabao20041230@gmail.com>
2022-04-15 17:56:46 +0800

    ver4

--- app.py
+++ app.py
@@ -12,10 +12,10 @@ def lineNotifyMessage(token, msg):
     r = requests.post("https://notify-api.line.me/api/notify", headers = headers, params = payload)
     return r.status_code
 
+line_msg=diagnosed_people('https://covid-19.nchc.org.tw/dt_005-covidTable_taiwan.php')
 
 if __name__ == "__main__":
-  line_msg=diagnosed_people('https://covid-19.nchc.org.tw/dt_005-covidTable_taiwan.php')
   total_people=line_msg[0]
-  token = 'oSVbmpG1KETazWdSmkkTgdpwu5QhiBDnIJNTw7sHjaS'
+  token = 'M8OZJMmL5p6BSOiBJZMt6f9gH4seLahLBF7GEIzXWvW'
   message = str(line_msg)  
   lineNotifyMessage(token, message)
